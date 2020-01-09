ba464fc3169070696b8cf7dacafa3dbbb9f4272e
James Nelson <johndoe@example.com>
2020-01-09 11:08:02 -0700

    5th commit

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(session({
 }))
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, '../build/'));
 });
 
 massive(CONNECTION_STRING).then(db => {
