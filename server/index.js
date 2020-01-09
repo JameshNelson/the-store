0f34a09971b173eef870839c33ee1469d5cc0130
James Nelson <johndoe@example.com>
2020-01-09 11:16:41 -0700

    added express.static for host

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ app.post("/charge", async (req, res) => {
 
 //Stripe
      
-
+app.use( express.static( `${__dirname}/../build` )); 
 
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '../build/index.html'));
